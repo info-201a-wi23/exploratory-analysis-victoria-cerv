11a938f9e99fa5ef99913c42bb07904f4e3036c3
MeganaShivakumar <megana.shivakumar@gmail.com>
2023-02-16 19:58:45 -0800

    more changes

--- Table.R
+++ Table.R
@@ -1,5 +1,9 @@
 library(dplyr)
 library(ggplot2)
 
-health <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-MeganaShivakumar/main/2.12_Health_systems.csv", stringsAsFactors = FALSE)
-
+health <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-victoria-cerv/main/2.12_Health_systems.csv", stringsAsFactors = FALSE)
+gdp <- health %>% 
+  select(`Health_exp_pct_GDP_2016`, `Health_exp_public_pct_2016`, `Health_exp_per_capita_USD_2016`, `Physicians_per_1000_2009.18`, `Nurse_midwife_per_1000_2009.18`) %>% 
+  group_by(`Health_exp_pct_GDP_2016`) %>% 
+  
+  kable(gdp)
