fefb5f6e9f68b8b82d0eb7a2a0db76ab4c3d57c3
MeganaShivakumar <megana.shivakumar@gmail.com>
2023-02-16 19:33:39 -0800

    2nd changes

---
+++ Table.R
@@ -0,0 +1,5 @@
+library(dplyr)
+library(ggplot2)
+
+health <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-MeganaShivakumar/main/2.12_Health_systems.csv", stringsAsFactors = FALSE)
+
