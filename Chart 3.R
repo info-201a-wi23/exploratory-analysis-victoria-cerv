b0145b218f5922e3e638355a767a8635d08271f4
Victoria Cervantes <your-email-address>
2023-02-20 11:39:17 -0800

    finished editing chart 3 and added chart 3 description

--- Chart 3.R
+++ Chart 3.R
@@ -1,7 +1,15 @@
+#loading relevant packages 
 library(dplyr)
+library("ggplot2")
 
+#read in relevant files 
 health_data <- read.csv("2.12_Health_systems.csv", stringsAsFactors = FALSE)
 
-max_country_exp <- (max(health_exp_per_capita_USD_2016)))
+#pulling top 10 countries 
+sorting_health <- health_data %>% arrange(-Health_exp_per_capita_USD_2016, na.rm=TRUE) %>% top_n(10, Health_exp_per_capita_USD_2016)
 
-ggplot(data = health_data) + geom_col(aes(x = Country_Region, y = Health_exp_per_capita_USD_2016), position = "dodge")
+
+#creating the chart 
+ggplot(data = sorting_health) + geom_col(aes(x = Country_Region, y = Health_exp_per_capita_USD_2016), position = "dodge") + labs(title = "Top 10 Countries Health Expenditure", x = "Top 10 Countries", y = "Health Expenditure per Capita (USD)")
+
+
